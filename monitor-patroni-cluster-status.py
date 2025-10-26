453220738554a27579be2e41cfc77266958f6030
sauravbilung <saurav.bilung@gmail.com>
2025-10-26 16:28:37 +0530

    fixes : indentation fixes, added cluster name : pg_cluster

--- monitor-patroni-cluster-status.py
+++ monitor-patroni-cluster-status.py
@@ -4,15 +4,15 @@ import subprocess
 NODES = ["pg_cluster-pg1-1", "pg_cluster-pg2-1", "pg_cluster-pg3-1"]
 
 for node in NODES:
-try:
-result = subprocess.run(
-["docker", "exec", node, "patronictl", "-c", "/etc/patroni.yml", "list"],
-stdout=subprocess.PIPE,
-stderr=subprocess.DEVNULL,
-text=True,
-check=True
-)
-print(result.stdout)
-break
-except subprocess.CalledProcessError:
-continue
+    try:
+        result = subprocess.run(
+            ["docker", "exec", node, "patronictl", "-c", "/etc/patroni.yml", "list"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            text=True,
+            check=True
+        )
+        print(result.stdout)
+        break
+    except subprocess.CalledProcessError:
+        continue
